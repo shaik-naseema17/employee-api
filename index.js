db50114377287fc7b842b23dee5d04f4626165ee
shaik-naseema17 <naseemas732@gmail.com>
2025-06-04 21:03:33 +0530

    added url

--- index.js
+++ index.js
@@ -13,21 +13,33 @@ import settingRouter from './routes/setting.js'
 import leaveRouter from './routes/leave.js'
 import dashboardRouter from "./routes/dashboard.js";
 
-const app=express()
+const app = express()
+const allowedOrigins = [
+    "https://employee-frontend1-one.vercel.app",
+    "http://localhost:5173"
+];
+
 app.use(cors({
-    origin:"https://employee-frontend1-one.vercel.app",
-    credentials:true
-}))
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true
+}));
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public/uploads'))
-app.use('/api/auth',authRouter)
-app.use('/api/department',departmentRouter);
-app.use("/api/employee",employeeRouter)
-app.use("/api/leave",leaveRouter)
-app.use('/api/setting',settingRouter)
-app.use('/api/salary',salaryRouter)
-app.use('/api/dashboard',dashboardRouter)
-app.listen(process.env.PORT,()=>{
+app.use('/api/auth', authRouter)
+app.use('/api/department', departmentRouter);
+app.use("/api/employee", employeeRouter)
+app.use("/api/leave", leaveRouter)
+app.use('/api/setting', settingRouter)
+app.use('/api/salary', salaryRouter)
+app.use('/api/dashboard', dashboardRouter)
+app.listen(process.env.PORT, () => {
     console.log(`server is running on the port ${process.env.PORT}`)
 })
