452d211f911414516e4cee48aafc6276c4e01cd1
shaik-naseema17 <naseemas732@gmail.com>
2025-06-06 12:25:47 +0530

    ad

--- routes/employee.js
+++ routes/employee.js
@@ -1,9 +1,15 @@
 import express from 'express'
 import authMiddleware from '../middleware/authMiddleware.js'
-import { addEmployee,upload,getEmployees,getEmployee,updateEmployee,fetchEmployeesByDepId} from '../controllers/employeeController.js'
+import { 
+    addEmployee, 
+    getEmployees, 
+    getEmployee, 
+    updateEmployee, 
+    fetchEmployeesByDepId 
+} from '../controllers/employeeController.js';
 const router=express.Router()
 
-router.post('/add',authMiddleware,upload.single("image"),addEmployee)
+router.post('/add',addEmployee)
 router.get('/',authMiddleware,getEmployees)
 router.get('/:id',authMiddleware,getEmployee)
 
